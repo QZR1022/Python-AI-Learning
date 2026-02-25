56340824160722297c13bda977e276670e74f1b1
qzr <2873214929@qq.com>
2026-02-25 22:01:00 +0800

    完成 day2 计算器代码

---
+++ test.py
@@ -0,0 +1,12 @@
+# Day02 输入、数字运算、简易计算器
+print("=== 简易计算器 ===")
+
+a = float(input("请输入第一个数字："))
+b = float(input("请输入第二个数字："))
+
+# 计算
+print("加法：", a + b)
+print("减法：", a - b)
+print("乘法：", a * b)
+print("除法：", a / b)
+print("取余：", a % b)
