6d1548277591bd47cef2494694cd7da3a4e983c1
qzr <2873214929@qq.com>
2026-02-25 22:33:22 +0800

    day5

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-# Day04 for 循环：求和、打印图案
-print("1 到 100 求和：")
-sum_result = 0
-for i in range(1, 101):
-    sum_result += i
-print("结果：", sum_result)
+# Day05 while 循环：猜数字游戏
+import random
 
-# 打印星星
-print("\n打印星星：")
-for i in range(1, 6):
-    print("*" * i)
+# 随机生成1-10的数字
+target_num = random.randint(1, 10)
+print("=== 猜数字游戏（1-10）===")
+
+while True:
+    guess = int(input("请猜一个数字："))
+
+    if guess > target_num:
+        print("太大了，再试试～")
+    elif guess < target_num:
+        print("太小了，再试试～")
+    else:
+        print("恭喜你！猜对啦！")
+        break
