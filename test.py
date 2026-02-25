c753191644d81dd62b3b000e0079f872d9d346bd
qzr <2873214929@qq.com>
2026-02-25 16:58:17 +0800

    first commit: day1 code

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hello Python！我开始学习啦～")
+
+# 变量
+name = "软件工程大一学生"
+age = 18
+print("我是：", name)
+print("年龄：", age)
+
+# 输入
+your_name = input("请输入你的名字：")
+print("你好，", your_name, "！一起学Python～")
